b1a89574cff1e870648dba9a7fe88d84f7c3522f
domieee <99103235+domieee@users.noreply.github.com>
2023-02-03 14:31:31 +0100

    added output

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,13 @@ const numberInput = document.querySelector('#numberInput')
 const loopFunction = () => {
     for (let i = 0; i <= numberInput.value; i++) {
         loop.splice(1, 0, 'o')
+
+        str = loop.toString()
+        
+        document.querySelector('#output').innerHTML = str.replaceAll(',', '')
+
     }
+
 }
 
 document.querySelector('#loopButton').addEventListener('click', loopFunction)
